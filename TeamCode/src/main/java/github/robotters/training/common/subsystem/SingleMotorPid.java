ee0d67da0815e741b022999baae1dc5ae931cded
foehammer127 <foehammer@disroot.org>
2023-09-09 20:44:03 -0500

    Implement DefaultLinearSlideCommand

---
+++ TeamCode/src/main/java/github/robotters/training/common/subsystem/SingleMotorPid.java
@@ -0,0 +1,22 @@
+package github.robotters.training.common.subsystem;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDFController;
+
+// Single Motor Pid Controller Stateful Subsystem
+public class SingleMotorPid extends SubsystemBase {
+    private PIDFController controller;
+
+    public SingleMotorPid(double kP, double kI, double kD, double kF) {
+        this.controller = new PIDFController(kP, kI, kD, kF);
+    }
+
+    public double GetOutput(int pos, int target) {
+        return controller.calculate(pos, target);
+    }
+
+    public void Reset() {
+        controller.reset();
+    }
+
+}
--- TeamCode/src/main/java/github/robotters/training/simba/subsystems/LinearSlide.java
+++ TeamCode/src/main/java/github/robotters/training/simba/subsystems/LinearSlide.java
@@ -2,9 +2,10 @@ package github.robotters.training.simba.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class LinearSlide extends SubsystemBase {
-    public Position position;
+    public Position target_position;
     public enum Position {
         UP,
         DOWN
@@ -13,11 +14,15 @@ public class LinearSlide extends SubsystemBase {
     private MotorEx slide_motor;
 
     public LinearSlide(MotorEx slide_motor) {
-        this.position = Position.DOWN;
+        target_position = Position.DOWN;
         this.slide_motor = slide_motor;
     }
 
     public void SetSpeed(double speed) {
         slide_motor.set(speed);
+    }
+
+    public int Get_Pos() {
+        return slide_motor.getCurrentPosition();
     }
 }
