8be260f06e64207946bdba8c28684fadef5518b3
foehammer127 <foehammer@disroot.org>
2023-09-11 22:06:34 -0500

    Create Logger Object

---
+++ TeamCode/src/main/java/github/robotters/training/common/logging/LogTopic.java
@@ -0,0 +1,11 @@
+package github.robotters.training.common.logging;
+
+public class LogTopic {
+    public String key;
+    public String value;
+
+    public LogTopic(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
